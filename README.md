df4c1169680bda3e96b1fe38d3dc60c63cc581cd
Kevin Kuek <kevin.kuek.95@gmail.com>
2017-05-17 00:46:14 +0800

    - add skeleton functions - general process working 	problems 	- prevBlockHash and minerKey returning dummy values 	- merkleTree just adding transactions together

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # CITS3002-Blockchain-Project
+
+# Block
+Header:
+- prevBlockHash
+- timeStamp
+- difficultyTarget
+- (extension) merkle root
+- nonce
+
+Body:
+- Coinbase transaction
+- Rest of transcations
+
+Only the header of the block is hashed to find the correct nonce, so the merkle root is what actually
+ties the transactions to the block and makes sure they were not changed.
+If we are not using a merkle root the entire block (header and body) should be hashed to tie the transcations
+to the hash.
+
+# Miner functions:
+Complete:
+- createCoinbaseTransaction(json[] transactions) : json coinbaseTransaction
+- generateBlockHeader(int difficulty, json[] transactions) : dict header
+- generateNonce(dict header) : dict headerWithNonce
+- checkNonce(dict header) : bool isValid
+
+Incomplete:
+- (returning dummy value) getMinerKey() : str minerKey
+- (probably not needed. Just added for completeness) generateBlockBody(json[] transactions) : json[] transactions
+- (just concatenating them rn) merkleRoot(json[] transactions) : str root
+- (returning dummy value) getLastBlockHash() : str lastBlockHash
+
+To use:
+    cb = createCoinbaseTransaction([transactions])
+    hd = generateBlockHeader([transactions, cb])
+    generateNonce(hd)
+See example
--- miner.py
+++ miner.py
@@ -1,59 +1,134 @@
-import hashlib, sys, time, random
+import hashlib, sys, time, random, json
 random.seed()
 
-text = 'example'
+#creates money out of nowhere for the miner and adds transaction fees
+#return json string of coinbase transaction
+def createCoinbaseTransaction(transactions):
+    fee = 0
+    for transaction in transactions:
+        transaction = json.loads(transaction)
+        fee += transaction['value'] - transaction['payment'] - transaction['change']
 
-difficulty = 20
+    coinbaseTransaction = {'reciever' : getMinerKey(), 'value' : fee, 'payment' : fee, 'change' : 0}
 
+    return json.dumps(coinbaseTransaction)
 
-def test(text, difficulty): 
-    nonce = random.randint(0,9999999999999)
+def getMinerKey():
+    return 'miner'
+
+#creates a header for the block without a nonce
+def generateBlockHeader(difficulty, transactions):
+    blockHeader = {'prevBlockHash':getLastBlockHash(), 'timeStamp':time.time(), 'difficultyTarget':difficulty, 'nonce':None, 'merkleRoot':merkleRoot(transactions)}
+    return  blockHeader
+
+def generateBlockBody(transactions):
+    return transactions
+
+#hash pair of transactions, add them together. Hash again
+#just returning array rn. Need to do properly
+def merkleRoot(transactions):
+    root = ''
+    for transaction in transactions:
+        root += transaction
+
+    return root
+
+#reference blockchain to get hash of previous block
+def getLastBlockHash():
+    #get last block
+    #hash it
+    #return hash
+    return '0000smileyface'
+
+def generateNonce(header):
     loop = True
     best = 0
     timer = time.time()
 
-
     while loop:
+        #generate random nonce
         nonce = random.randint(0,9999999999999)
 
-        val = (text + str(nonce)).encode('utf-8')
+        header['nonce'] = nonce
 
-        h = hashlib.sha256()
-        h.update(val)
-        h = h.digest()
+        #combine input and nonce
+        val = (json.dumps(header)).encode('utf-8')
 
+        #hash header-nonce combination
+        hash = hashlib.sha256()
+        hash.update(val)
+        digest = hash.digest()
 
+        #create string to hold bits as int rather than byte type
         bits = ''
 
-        for i in h:
+        #takes bits one at a time and convert to 8-bit binary string
+        for i in digest:
+            #add binary string representation of byte into bits string
             bits += bin(i)[2:].zfill(8)
 
         for i in range(0,difficulty + 1):
+            #check if value of bit is 0
             if int(bits[i]) == 0:
+                #This whole section is just to help visualise progress
+                # write current highest bit reached, to help visualise progress. Not necessary
                 if int(i) > best:
                     best = int(i)
                 sys.stdout.write('Best: %d  \r' % best)
                 sys.stdout.flush()
 
+                #if you have a run of 0s long enough then break out of the loop
                 if int(i) == difficulty:
                     loop = False
-
+            #if this bit isnt zero move one to next nonce
             else:
                 break
 
-
+    #print time taken
     print(time.time() - timer)
     print('successful nonce: ', nonce)
-    return nonce
+    return header
 
-checkNonce = test(text, difficulty)
+#take hash as input to check against
+def checkNonce(header):
+    hash = hashlib.sha256()
+    check = (json.dumps(header)).encode('utf-8')
+    hash.update(check)
+    digest = hash.digest()
+    bits = ''
+    #change so prints in bits and returns true if correct
+    for i in digest:
+        # add binary string representation of byte into bits string
+        bits += bin(i)[2:].zfill(8)
+    print(bits)
 
-h = hashlib.sha256()
-check = (text + str(checkNonce)).encode('utf-8')
-h.update(check)
-h = h.digest()
-print('====Check Nonce====')
-print('hash : ', h)
-print('bytes: ',h[0],h[1],h[2],h[3],h[4],h[5],h[6],h[7],h[8])
+    #count leading 0s
+    count = 0
+    for bit in bits:
+        if bit == '0':
+            count += 1
+        else:
+            break
+    #return true if valid
+    if count >= header['difficultyTarget']:
+        return True
+    #return False if not valid
+    return False
+
+
+
+"""example"""
+difficulty = 10
+
+#example transaction
+transaction = {'sender':'Alice', 'receiver':'Bob', 'value':50, 'payment': 40, 'change':5, 'signature':'u4h298h4g92'}
+transactionString = json.dumps(transaction)
+
+coinbaseTransaction = createCoinbaseTransaction([transactionString])
+
+header = generateBlockHeader(difficulty, [transactionString, coinbaseTransaction])
+headerWithNonce = generateNonce(header)
+print(header)
+print(checkNonce(header))
 
 
