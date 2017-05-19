12371631bd4aa8664759f6220a302814542b403d
Unknown <21507301@student.uwa.edu.au>
2017-05-19 23:04:44 +0800

    Got verification working

--- keyTest.py
+++ keyTest.py
@@ -1,12 +1,11 @@
 import keyUtils
+import pytest
 
 message = "TESTING"
 name = "jane"
 (public_key,private_key) = keyUtils.generateKeys()
 
-def signatureTest():
+def test_signature():
     signature = keyUtils.generateSign(message,private_key)
-    assert(keyUtils.verifyMessage(message,signature,public_key)) == True
-    assert(keyUtils.verifyMessage('NOT TESTING',signature,public_key)) == False
-
-signatureTest()
+    assert keyUtils.verifyMessage(message,signature,public_key) == True
+    assert keyUtils.verifyMessage('NOT TESTING',signature,public_key) == False
