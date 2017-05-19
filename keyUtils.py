956472e87bd08d6e36add74623c7eda6abdc20ad
Unknown <21507301@student.uwa.edu.au>
2017-05-19 12:26:39 +0800

    Addded signature and verification of signatures

--- keyUtils.py
+++ keyUtils.py
@@ -1,7 +1,19 @@
 import rsa
 
 #Generate Privatekey, random
+def generateKeys():
+    return rsa.newkeys(2048)
 
-(private_key , public_key) = rsa.newkeys(2048)
+def generateSign(message,key):
+    return rsa.sign(message, key, 'SHA-256')
+
+def verifyMessage(message,signature,key):
+    try:
+        rsa.verfiy(message,signature,key)
+        return True
+    except Exception as e:
+        print('VERFICATION FAILED')
+        return False
+
 print(private_key)
 print(public_key)
