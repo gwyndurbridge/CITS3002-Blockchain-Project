cec17d2d5f1a443581e104c4e878193a12665af2
Unknown <21507301@student.uwa.edu.au>
2017-05-19 23:53:03 +0800

    FInalised unit testing for keyUtils

--- keyTest.py
+++ keyTest.py
@@ -9,3 +9,17 @@ def test_signature():
     signature = keyUtils.generateSign(message,private_key)
     assert keyUtils.verifyMessage(message,signature,public_key) == True
     assert keyUtils.verifyMessage('NOT TESTING',signature,public_key) == False
+
+def test_encryption():
+    encryptedMessage = keyUtils.encryptMessage(message,public_key)
+    assert keyUtils.decryptMessage(encryptedMessage,private_key) == message
+    encryptedMessage = encryptedMessage[:-1] + b'X'
+    assert keyUtils.decryptMessage(encryptedMessage,private_key) == ''
+
+def test_PEMcreation():
+	keyUtils.generatePEM(public_key,name,True)
+	readKey = keyUtils.readPEM(name,True)
+	assert readKey == public_key
+	keyUtils.generatePEM(private_key,name,False)
+	readKeyPrivate = keyUtils.readPEM(name,False)
+	assert readKeyPrivate == private_key
