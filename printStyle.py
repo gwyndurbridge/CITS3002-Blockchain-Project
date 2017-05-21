d2cca0993fa00747543f8e6bd756a7232519f836
gwyndurbridge <gwyndurbridge@gmail.com>
2017-05-21 17:02:02 +0800

    added function to print title strings

--- printStyle.py
+++ printStyle.py
@@ -6,3 +6,14 @@ MONEY_BAG = u"\U0001F4B0"
 BOLD = "\033[1m"
 PURPLE = "\033[95m"
 RESET = "\033[0m"
+
+
+def title(input_string):
+    """
+    Returns title message with print style effects
+    """
+    output = "\n" + MONEY_BAG + BOLD + PURPLE + " "
+    output += input_string
+    output += " " + RESET + MONEY_BAG + "\n"
+    return output
+    
