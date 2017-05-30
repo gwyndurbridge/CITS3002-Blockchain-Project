8712cdd89ad74e0b0bbcc9c57aa3fef32c161569
gwyndurbridge <gwyndurbridge@gmail.com>
2017-05-30 09:52:53 +0800

    some progress...

--- s11.py
+++ s11.py
@@ -1,6 +1,8 @@
 """
 https://gist.github.com/messa/22398173f039d1230e32
 https://docs.python.org/3/library/multiprocessing.html#module-multiprocessing
+
+http://www.giantflyingsaucer.com/blog/?p=5557
 """
 
 import asyncio
@@ -19,7 +21,11 @@ def startServer():
     server_process = multiprocessing.Process(target=server, name='server',args=(data, q))
     server_process.start()
     print("process name: ", server_process.name)
-
+    queue_process = multiprocessing.Process(target=myqueue, name='myqueue',args=(q,))
+    queue_process.start()
+    print("process name: ", queue_process.name)
+    
+        
 def startClient():
     print("Starting as client")
     assert os.path.isfile('selfsigned.cert')
@@ -44,41 +50,22 @@ def getTask():
             getTask()
 
 def main():
-    # Command to generate the self-signed key:
-    #
-    #     openssl req -x509 -newkey rsa:2048 -keyout selfsigned.key -nodes \
-    #                 -out selfsigned.cert -sha256 -days 1000
-    #
-    # use 'localhost' as Common Name
-
-    # assert os.path.isfile('selfsigned.cert')
-    # server_process = multiprocessing.Process(target=server, name='server')
-    # server_process.start()
-    # try:
-    #     sleep(.5)
-    #     client_process = multiprocessing.Process(target=client, name='client')
-    #     client_process.start()
-    #     try:
-    #         client_process.join(1)
-    #         assert not client_process.is_alive()
-    #     finally:
-    #         client_process.terminate()
-    #         client_process.join()
-    # finally:
-    #     server_process.terminate()
-    #     server_process.join()
     getTask()
 
 def server(data, q):
 
-    print('data: ', data)
-    print("queue: ", q)
+    # print('data: ', data)
+    # print("queue: ", q.get())
 
 
     @asyncio.coroutine
     def handle_connection(reader, writer):
+        """
+        handle correction
+        """
         addr = writer.get_extra_info('peername')
         data = yield from reader.readline()
+        q.put([{"data":data}])
         print("Server received {!r} from {}".format(data, addr))
         assert data == b'ping\n', repr(data)
         writer.write(b'pong\n')
@@ -90,7 +77,7 @@ def server(data, q):
     sc.load_cert_chain('selfsigned.cert', 'selfsigned.key')
 
     loop = asyncio.get_event_loop()
-    coro = asyncio.start_server(handle_connection, '127.0.0.1', port, ssl=sc, loop=loop)
+    coro = asyncio.start_server(handle_connection, 'localhost', port, ssl=sc, loop=loop)
     server = loop.run_until_complete(coro)
 
     print('Serving on {}'.format(server.sockets[0].getsockname()))
@@ -103,11 +90,9 @@ def client():
     @asyncio.coroutine
     def tcp_echo_client(loop):
         print("something2")
-        sc = ssl.create_default_context(ssl.Purpose.SERVER_AUTH,
-            cafile='selfsigned.cert')
+        SC = ssl.create_default_context(ssl.Purpose.SERVER_AUTH, cafile='selfsigned.cert')
 
-        reader, writer = yield from asyncio.open_connection(
-            'localhost', port, ssl=sc, loop=loop)
+        reader, writer = yield from asyncio.open_connection('localhost', port, ssl=SC, loop=loop)
         writer.write(b'ping\n')
         yield from writer.drain()
         data = yield from reader.readline()
@@ -120,6 +105,21 @@ def client():
     loop.run_until_complete(tcp_echo_client(loop))
     loop.close()
 
+def myqueue(q):
+
+    @asyncio.coroutine
+    def check_queue(q, loop):
+        """
+        check queue
+        """
+        print("QUEUE:", q.get())
+
+    loop = asyncio.get_event_loop()
+    # coro = asyncio.check_queue(check_queue(q, loop))
+    loop.run_forever(check_queue(q, loop))
+
+    print("Starting queue checker...")
+    loop.run_forever()
 
 if __name__ == '__main__':
     main()
