6c7c38ecbe9079a50603fad25e63eeecdbd73f88
gwyndurbridge <gwyndurbridge@gmail.com>
2017-06-02 17:24:55 +0800

    added networking files

---
+++ networkclient.py
@@ -0,0 +1,17 @@
+import asyncio
+import ssl
+
+class Client:
+    def __init__(self, name, callback):
+        self.name = name
+    
+    self.callback = callback
+
+    def test:
+        self.callback("send me back")
+
+# balance 
+# send coins (name, amount to send, change)
+
+{'pending':True,
+'transaction':Transa,}
