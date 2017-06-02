b821174fa6e76ce798175354e5ea551fb0c422a8
dneil <daniel.n.csc@gmail.com>
2017-06-02 16:40:23 +0800

    CLI->Wallet working
    Signed-off-by: dneil <daniel.n.csc@gmail.com>

--- cli.py
+++ cli.py
@@ -1,16 +1,16 @@
 import json
 import os
-import random
 import re
 
-# import Wallet
-# import miner
+from client import Client
+
 nameCol = "0;0;0"
 base = "0"
 green = "0;32;0"
 purple = "0;35;0"
 orange = "0;33;0"
 red = "0;31;0"
+blue = "0;34;0"
 testTrans = [{'sender': 'Andy', 'receiver': 'jerry', 'value': 50, 'payment': 40, 'change': 5,
               'signature': 'u4h298h4g92'},
              {'sender': 'James', 'receiver': 'Billy', 'value': 35, 'payment': 10, 'change': 15,
@@ -118,60 +118,98 @@ def helpWallet():
             red, 'exit'))
         cont = input("  press any key to continue: ")
 
-def walletCLI(init):
-    # act = Wallet(init)
+
+def walletCLI(init, fname):
+    act = Client(init, False)
     while True:
         print("Available Functions: ")
-        print("1. \x1b[%sm %19s \x1b[0m" % (green, 'help;'))
-        print("2. \x1b[%sm %19s \x1b[0m" % (green, 'whoami'))
-        print("3. \x1b[%sm %19s \x1b[0m" % (purple, 'checkBalance;'))
-        print("4. \x1b[%sm %19s \x1b[0m" % (orange, 'sendCoins;'))
-        print("5. \x1b[%sm %19s \x1b[0m" % (red, 'exit;'))
+        print("1. \x1b[%sm %17s \x1b[0m" % (green, 'help;'))
+        print("2. \x1b[%sm %17s \x1b[0m" % (green, 'whoami'))
+        print("3. \x1b[%sm %17s \x1b[0m" % (purple, 'checkBalance;'))
+        print("4. \x1b[%sm %17s \x1b[0m" % (orange, 'sendCoins;'))
+        print("5. \x1b[%sm %17s \x1b[0m" % (red, 'exit;'))
         var = input("Please choose from the above functions: ")
+        print("")
         if var == "help" or var == "1":
             helpWallet()
+            print("")
         elif var == "checkBalance" or var == "3":
             print("Checking your balance")
             print("Account: \x1b[%sm %10s \x1b[0m" % (purple, init))
-            # print("Available: %20s" % act.availableFunds())
-            # print("Total Balance: %20s" % act.actualBalance())
+            x, y = act.check_balance()
+            print("Total: %13s \nAvailable: %9s\n" % (x, y))
         elif var == "sendCoins" or var == "4":
-            f = []
-            for name in f:
+            print("Collecting relevant information")
+            for name in fname:
                 if name != init:
-                    print("name: %s" % name)
+                    print("\x1b[%sm name | %s \x1b[0m" % (blue, name))
             recipient = input("Please enter the recipient: ")
+            if recipient.strip() == 'break' or recipient.strip() == '':
+                continue
             """This loop ensures that you are not entering an invalid name"""
-            while not (recipient in f) & (recipient != init):
-                for name in f:
+            while not (recipient in fname) & (recipient != init):
+                for name in fname:
                     if name != init:
-                        print("name: %s" % name)
+                        print("\x1b[%smname: %s\x1b[0m" % (blue, name))
                 recipient = input("Please enter one of the names listed above for the recipient: ")
+                if recipient.strip() == 'break' or recipient.strip() == '':
+                    break
+            if recipient.strip() == 'break' or recipient.strip() == '':
+                continue
             value = input("Please enter how much you are putting into the transaction: ")
-            while not (isnum(value)) or float(value) <= 0:
-                value = input(
-                    "Please enter a numerical amount, greater than 0 that you are putting into the transaction: ")
+            if value.strip() == 'break' or value.strip() == '':
+                continue
+            chuck, available = act.check_balance()
+            while not (isnum(value)) or float(value) <= 0 or float(value) > available:
+                if not isnum(value):
+                    value = input(
+                        "Please enter a numerical amount: ")
+                elif float(value) <= 0:
+                    value = input("Please enter a value greater than 0: ")
+                elif float(value) > available:
+                    value = input(
+                        "You only have %s in your account, please enter a value less than or equal to this: " % available)
+                if value.strip() == 'break' or value.strip() == '':
+                    break
+            if value.strip() == 'break' or value.strip() == '':
+                continue
             payment = input("Please enter how much you are sending to %s: " % recipient)
+            if payment.strip() == 'break' or payment.strip() == '':
+                continue
             while not (isnum(payment)) or float(payment) > float(value) or float(payment) <= 0:
-                payment = input(
-                    "Please enter a numerical value lower than or equal to %s but greater than 0, that you are paying to %s: " % (
-                        value, recipient))
+                if not isnum(payment):
+                    payment = input(
+                        "Please enter a numerical value that you are paying to %s: " % (
+                            recipient))
+                elif float(payment) <= 0:
+                    payment = input(
+                        "Please enter a numerical value greater than 0 that you are paying to %s" % recipient)
+                elif float(payment) > float(value):
+                    payment = input(
+                        "Please enter a numerical value less than %s that you are paying to %s" % (value, recipient))
+                if payment.strip() == 'break' or payment.strip() == '':
+                    break
+            if payment.strip() == 'break' or payment.strip() == '':
+                continue
             minerFee = input("Please enter how much you are paying the miner: ")
+            if minerFee.strip() == 'break' or minerFee.strip() == '':
+                continue
             while not (isnum(minerFee)) or float(minerFee) > (float(value) - float(payment)) or float(
                     minerFee) < 0:
                 minerFee = input(
                     "Please enter a numerical value between 0 and %d, that you are paying to the miner: " % (
                         float(value) - float(payment)))
+                if minerFee.strip() == 'break' or minerFee.strip() == '':
+                    break
+            if minerFee.strip() == 'break' or minerFee.strip() == '':
+                continue
             change = float(value) - float(payment) - float(minerFee)
             print(
                 "You are creating a transaction with a value of %s, with %s being sent to %s and %s being sent to the miner. You will be receiving %s as change"
                 % (value, payment, recipient, minerFee, change))
-            '''actual.generateTransaction(recipient, value, payment, change)'''
+            print("Success: %s\n" % act.send_transaction(recipient, value, payment, change))
         elif var == "whoami" or var == "2":
-            cont = 'n'
-            while cont != 'y' and cont != 'Y':
-                print("You are: %20s" % init)
-                cont = input("continue? Y/N: ")
+            print("\x1b[%sm You are: %20s \x1b[0m\n" % (blue, init))
         elif var == "exit" or var == "5":
             break
         else:
@@ -182,31 +220,31 @@ def walletCLI(init):
 def main():
     exit = 'N'
     while exit != 'Y' and exit != 'y':
-        print("Known accounts: ")
-        f = []
-        for (dirpath, dirnames, filenames) in os.walk(os.getcwd()):
-            for filename in filenames:
-                a = re.findall('^(\w+)Public\.pem$', filename)
-                if not a:
-                    continue
-                f += a
-        var = []
-        for name in f:
-            var.append("0;" + str(random.randint(30, 37)) + ";0")
-            print("name \x1b[%sm | %10s \x1b[0m" % (var[-1], name))
-        init = input("Who are you signing in as? ")
-        while init not in f:
-            init = input("Please choose one of the names above: ")
-        nameCol = var[f.index(init)]
-        print("At any choice-junction you may input 'whoami' to find out who you are signed in as.")
         while True:
-            start = input("Would you like to start a miner or a wallet? ")
+            start = input(
+                "Would you like to start a \x1b[%smminer\x1b[0m or a \x1b[%smwallet\x1b[0m? " % (purple, green))
             if start == "wallet":
-                walletCLI(init)
+                print("Known accounts: ")
+                f = []
+                for (dirpath, dirnames, filenames) in os.walk(os.getcwd()):
+                    for filename in filenames:
+                        a = re.findall('^(\w+)Public\.pem$', filename)
+                        if not a:
+                            continue
+                        f += a
+                for name in f:
+                    print("\x1b[%sm name | %10s \x1b[0m" % (blue, name))
+                init = input("Who are you signing in as? ")
+                while init not in f:
+                    init = input("Please choose one of the names above: ")
+                print("At most points in this program you may input 'whoami' to find out who you are signed in as.\n")
+                print("\n%s\n" % '--------------------')
+                walletCLI(init, f)
             elif start == "miner":
+                print("\n%s\n" % '--------------------')
                 minerCLI(init)
             elif start == "whoami":
-                print("You are: \x1b[%sm %10s \x1b[0m" % (nameCol, init))
+                print("You are: \x1b[%sm %10s \x1b[0m" % (blue, init))
             elif start == "exit":
                 break
             else:
--- client.py
+++ client.py
@@ -1,71 +1,55 @@
-import pprint
-import socket
-import ssl
+class Client:
+    def __init__(self, name, is_miner):
+        """
+        name: (string) wallet name
+        is_miner: (boolean) is client for miner
+        """
+        self.name = name
+        self.is_miner = is_miner
 
-import printStyle as ps
+    """
+    Wallet related functions
+    """
 
-'''
-init(name or pem file directory)
-getBal() returns actual/available balance - split in two if you want
-sendCoins(recipient, amountAvail, amountSent, amountChange) returns True if it was sent successfuly (maybe?)
-        amountAvail = trans $$$ input
-        amountSent = recipient $$$ received
-        amountChange = sender $$$ received back
-        you shouldn't need the sender's name as that should come from init()
-        I'm happy to format this differently (i.e. as a dict or a json.dump() or a json) if you want
-            but the values being inputted shouldn't need to change
+    def check_balance(self):
+        """
+        total, pending (leftover unprocessed i.e. total = 50, sent (unprocessed) = 30 therefore pending = 20)
+        """
+        return (50, 20)
 
-I don't think I'll need to get anything else from the wallet.
-Maybe a transaction history? that'd probably be an extension task if you want to implement it
-    just pass it back as a list of strings (i.e. json.dump()?) that I can run through to print out
-'''
+    def send_transaction(self, receiver, value, payment, change):
+        """
+        reciever: (string) name of receiver
+        value: (number) total amount to send from wallet
+        payment: (number) how much the receiver recieves
+        change: (number) how much the sender recieves
 
-def createSocket(host, port):
-    print("Creating socket with: " + host + ":", port)
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    ssl_sock = ssl.wrap_socket(s, ca_certs="cert.pem", cert_reqs=ssl.CERT_REQUIRED)
-    ssl_sock.connect((host, port))
+        Returns (string) message to print in CLI
+        """
+        return "Success"
 
-    print(repr(ssl_sock.getpeername()))
-    print(ssl_sock.cipher())
-    print(pprint.pformat(ssl_sock.getpeercert()))
+    """
+    Miner related functions
+    """
 
-    ssl_sock.write("Test Message!".encode())
+    def start_miner(self):
+        """
+        Returns (string) message to print in CLI
+        """
+        return "Success"
 
-    if False:  # from the Python 2.7.3 docs
-        # Set a simple HTTP request -- use httplib in actual code.
-        ssl_sock.write("""GET / HTTP/1.0\r
-        Host: www.verisign.com\n\n""")
+    def get_miner_difficulty(self):
+        """
+        Returns (Number) difficulty that miner is set to
+        """
+        return 35
 
-        # Read a chunk of data.  Will not necessarily
-        # read all the data returned by the server.
-        data = ssl_sock.read()
-
-        # note that closing the SSLSocket will also close the underlying socket
-        ssl_sock.close()
-
-
-def send(data):
-    print("Attempting to send...")
-
-
-def main():
-    # Welcome message
-    print("\n" + ps.MONEY_BAG + ps.BOLD + ps.PURPLE + " CITS3002 CLIENT " + ps.RESET + ps.MONEY_BAG + "\n")
-
-    # Request the host and port number of the serverfrom the user
-    host = input("Please enter the HOST address of the server: ")
-    port = input("Please enter the PORT number you wish to use: ")
-
-    # Use default values if no info is given
-    if len(host) == 0:
-        host = "127.0.0.1"  # localhost
-    if len(port) == 0:
-        port = 9999  # default port
-
-    # Create the socket with the given user info
-    createSocket(host, port)
+    def set_miner_difficulty(self, difficulty):
+        """
+        Returns (Number) difficulty that miner is set to
+        """
+        return "Successfully set difficulty to " + str(difficulty)
 
 
-if __name__ == '__main__':
-    main()
+c = Client("alice", False)
+print(c.set_miner_difficulty(22))
